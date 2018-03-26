ac8e292689779bc7923bf1e72b6cb04a8d0a3bf4
Pouya Samadi <pspouya90@gmail.com>
2018-03-26 21:40:32 +0200

    add custom sorting

--- ng2-cribs/src/app/app.module.ts
+++ ng2-cribs/src/app/app.module.ts
@@ -9,21 +9,27 @@ import { CribListingComponent } from './crib-listing/crib-listing.component';
 import { CribCardComponent } from './crib-card/crib-card.component';
 
 import { CribsService } from './services/cribs.service';
-import { AddListingFormComponent } from './add-listing-form/add-listing-form.component'; 
+import { UtilService } from './services/util.service';
+import { AddListingFormComponent } from './add-listing-form/add-listing-form.component';
+import { SortByPipe } from './pipes/sort-by.pipe'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     CribListingComponent,
     CribCardComponent,
-    AddListingFormComponent
+    AddListingFormComponent,
+    SortByPipe
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule
   ],
-  providers: [CribsService],
+  providers: [
+    CribsService,
+    UtilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ng2-cribs/src/app/crib-listing/crib-listing.component.ts
+++ ng2-cribs/src/app/crib-listing/crib-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { CribsService } from './../services/cribs.service';
+import { UtilService } from './../services/util.service';
 
 
 @Component({
@@ -12,11 +13,22 @@ export class CribListingComponent implements OnInit {
 
   cribs: Array<any>;
   error: string;
+  sortField: string = 'price';
+  sortDirection: string =  'asc';
+  sortFields: Array<string> = [
+    'address',
+    'area',
+    'bathrooms',
+    'bedrooms',
+    'price',
+    'type'
+  ];
 
   constructor(
     private http: Http,
-    private cribsService: CribsService){ }
-
+    private cribsService: CribsService,
+    private utilService: UtilService
+  ){ }
   ngOnInit() {
 
       this.cribsService.getAllCribs()
